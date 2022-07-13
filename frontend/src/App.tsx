066078bd4036a601510972db7c39210fa80ab35d
Vinicius <vinilean43@gmail.com>
2022-07-12 22:33:57 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return (
-    <h1>Olá Mundo</h1>
-  ) 
+    <>
+      <h1>Olá Mundo</h1>
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
